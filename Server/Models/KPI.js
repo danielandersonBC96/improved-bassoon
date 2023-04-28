99c5643daed3472da64a6738af0ac51508b7a529
danielandersonBC96 <damielandersonbrandaocampos@hotmail.com>
2023-04-28 16:05:45 -0400

    Key Pergomace Indications Route

--- Server/Models/KPI.js
+++ Server/Models/KPI.js
@@ -4,11 +4,84 @@ import {loadType} from "mongoose-currency"
 const Pages = mongoose.Pages;
 loadType(mongoose);
 
+const dayPages = new Pages(
+    {
+        data :String,
+        revenue:{
+            type: mongoose.Types.Currency,
+            currancy:"USD",
+            get:(v) => v / 100
+        },
+        expenses:{
+            type: mongoose.Types.Currency,
+            currancy:"USD",
+            get:(v) => v / 100
+        },
+    },
+   
+    {toJSON: {getters : true }}
+)
+
+const monthPages =  new Pages(
+    {
+        month :String,
+        revenue:{
+            type: mongoose.Types.Currency,
+            currancy:"USD",
+            get:(v) => v / 100
+        },
+        expenses:{
+            type: mongoose.Types.Currency,
+            currancy:"USD",
+            get:(v) => v / 100
+        },
+        operationalExpenses:{
+            type: mongoose.Types.Currency,
+            currancy:"USD",
+            get:(v) => v / 100
+        },
+          nonOperationExpenses :{
+            type: mongoose.Types.Currency,
+            currancy:"USD",
+            get:(v) => v / 100
+        },
+    },
+
+    {toJSON:{
+        getters: true
+    }}
+)
+
+
 const KPIPages = new Pages(
     {
         totalProfit: {
             type: mongoose.Types.Currency,
-        }
+            currancy:"USD",
+            get:(v) => v / 100
+        },
+        totalRevenue: {
+            type: mongoose.Types.Currency,
+            currancy:"USD",
+            get:(v) => v / 100
+        },
+        totalExpense : {
+            type: mongoose.Types.Currency,
+            currancy:"USD",
+            get:(v) => v / 100
+        },
+        expenseByCategory: {
+            type:Map,
+            of: {
+                 type: mongoose.Types.Currency,
+                 currancy:"USD",
+                 get:(v) => v / 100
+
+            }
+        },
+        monthlyData:[ monthPages ], 
+        dailyData:[dayPages]
+
     }
 
 );
