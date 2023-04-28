42445f4a948692cb2599a9fabddc65a167d7a449
danielandersonBC96 <damielandersonbrandaocampos@hotmail.com>
2023-04-28 12:37:45 -0400

    create server and conection DataBase

--- Server/index.js
+++ Server/index.js
@@ -4,9 +4,8 @@ import cors from 'cors';
 import helmet  from 'helmet';
 import mongoose from 'mongoose';
 import morgan from "morgan";
-import * as dotenv from 'dotenv' ;
-
-/* SETTINGS */
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+/* SETTINGS  SERVER */
 
 dotenv.config()
 const app = express();
@@ -15,11 +14,19 @@ app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy:"cross-origin"}));
 app.use(morgan('common'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extends: false}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 
-console.log ("hello world")
 
-/* MOGOOSE SETUP */
+/* MOGOOSE SETUP */ 
 
-
+const PORT = process.env.PORT || 4000;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(async () => {
+   app.listen(PORT, () => console.log(`SERVER PORT ${PORT}`))
+  })
+  .catch((error) =>  console.log(`${error} did not connect`))
